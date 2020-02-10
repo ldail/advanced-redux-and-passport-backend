680a301a4793567348e3d7fe711929c78967b011
ldail <leondailofficial@gmail.com>
2020-02-09 17:20:03 -0800

    fixed await

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,31 +2,11 @@ import {AUTH_USER, AUTH_ERROR} from './types';
 import Axios from 'axios';
 
 
-export const signUp = (formProps, callback) => dispatch => {
+export const signUp = (formProps, callback) => async dispatch => {
   try {
-    Axios.post('http://localhost:3090/signup', formProps)
-      .then(response => {
-        console.log('response starting');
-          console.log(response);
-          return response;
-        })
-      .then(resJson => dispatch({type: AUTH_USER, payload: resJson.data.token}))
-      .then(() => callback())
-      .catch(error => {
-        console.log('error in here');
-        dispatch({type: AUTH_ERROR, payload: 'Email in use'})
-      });
-    
-  //   let newResponse = response.json();
-  //   console.log(newResponse);
-  //   return newResponse;});
-  // console.log('passed the await');
-  // if (!response.data.error) {
-  //   dispatch({type: AUTH_USER, payload: response.data.token})
-  // }
-  // else {
-  //   dispatch({type: AUTH_ERROR, payload: 'Email in use'})
-  // }
+    const response = await Axios.post('http://localhost:3090/signup', formProps)
+    await dispatch({type: AUTH_USER, payload: response.data.token});
+    callback();
   }
   catch(error) {
     dispatch({type: AUTH_ERROR, payload: 'Email in use'})
